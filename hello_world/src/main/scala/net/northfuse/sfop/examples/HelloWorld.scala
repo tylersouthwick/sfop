9ce794fdbdaa0c8d486a013f108f01f2c7ffacbd
Tyler Southwick <tyler@northfuse.net>
2011-05-30 17:00:57 -0600

    working on hello world test

--- hello_world/src/main/scala/net/northfuse/sfop/examples/HelloWorld.scala
+++ hello_world/src/main/scala/net/northfuse/sfop/examples/HelloWorld.scala
@@ -2,6 +2,25 @@ package net.northfuse.sfop.examples
 
 import net.northfuse.sfop._
 
-class HelloWorld extends BasicReport {
-	def buildRegionBody = <block>Hello world!</block>
+object HelloWorld extends FOPDocument {
+import FOPDocument.foNamespace
+val name = "somebody"
+	
+	content += new Content {
+		val name = "test"
+
+		val layout = new SimplePageMaster {
+			val pageHeight = "29.7cm"
+			val pageWidth = "21.0cm"
+			val margin = "2cm"
+		}
+
+		val content = PageSequence {
+			<fo:flow flow-name="xsl-region-body">
+				<fo:block>
+					Hello, {name}!
+				</fo:block>
+			</fo:flow>
+		}
+	}
 }
--- hello_world/src/main/scala/net/northfuse/sfop/examples/Main.scala
+++ hello_world/src/main/scala/net/northfuse/sfop/examples/Main.scala
@@ -3,5 +3,10 @@ package net.northfuse.sfop.examples
 object Main {
 	def main(args : Array[String]) {
 		println("Hello world!")
+
+		import java.io._
+		val out = new FileOutputStream(new File("test.pdf"))
+		HelloWorld.render(out)
+		out.close()
 	}
 }
